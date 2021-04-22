a073a75b144e1bf866a20a744ed38f59ab47c5eb
SAN <maddamsettysanjit@gmail.com>
2021-04-22 11:59:06 +0530

    banner change if more clubs part of

--- src/uibits/BannerToPushToStartClub.js
+++ src/uibits/BannerToPushToStartClub.js
@@ -3,29 +3,51 @@ import {StyleSheet, View, Text, Dimensions} from 'react-native';
 import {Button} from 'react-native-elements';
 import FastImage from 'react-native-fast-image';
 import {useNavigation} from '@react-navigation/native';
+import {connect} from 'react-redux';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
-export default function BannerToPushToStartClub() {
+var state_here = {};
+
+function BannerToPushToStartClub() {
   const navigation = useNavigation();
 
-  return (
-    <View style={styles.banner_view}>
-      <FastImage
-        style={styles.banner_image}
-        source={require('/Users/san/Desktop/toastgo/assets/dog_3d.png')}
-      />
+  if (state_here.MyClubsReducer.myclubs.length <= 3) {
+    return (
+      <View style={styles.banner_view}>
+        <FastImage
+          style={styles.banner_image}
+          source={require('/Users/san/Desktop/toastgo/assets/dog_3d.png')}
+        />
+        <Button
+          buttonStyle={styles.start_club_button_style}
+          containerStyle={styles.start_club_button_container_style}
+          titleStyle={styles.start_club_button_title_style}
+          title="start club"
+          onPress={() => navigation.navigate('StartClub')}
+        />
+      </View>
+    );
+  } else {
+    return (
       <Button
-        buttonStyle={styles.start_club_button_style}
-        containerStyle={styles.start_club_button_container_style}
-        titleStyle={styles.start_club_button_title_style}
+        buttonStyle={styles.start_club_button_style_more}
+        containerStyle={styles.start_club_button_container_style_more}
+        titleStyle={styles.start_club_button_title_style_more}
         title="start club"
         onPress={() => navigation.navigate('StartClub')}
       />
-    </View>
-  );
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  state_here = state;
+  return state_here;
+};
+
+export default connect(mapStateToProps)(BannerToPushToStartClub);
 
 const styles = StyleSheet.create({
   banner_view: {
@@ -64,5 +86,30 @@ const styles = StyleSheet.create({
     width: 160,
     borderRadius: 30,
     backgroundColor: '#FFF',
+  },
+
+  start_club_button_title_style_more: {
+    fontFamily: 'GothamRounded-Medium',
+    fontSize: 17,
+    color: '#FFFFFF',
+  },
+  start_club_button_container_style_more: {
+    alignSelf: 'center',
+    marginVertical: windowHeight * 0.1,
+    backgroundColor: 'transparent',
+    shadowColor: '#36b37e',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 40,
+    elevation: 2,
+  },
+  start_club_button_style_more: {
+    height: 60,
+    width: 160,
+    borderRadius: 30,
+    backgroundColor: '#36b37e',
   },
 });
