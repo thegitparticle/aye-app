0bd5ee6d90c40f25cbf75bd6d537619424878307
SAN <maddamsettysanjit@gmail.com>
2021-05-01 21:30:01 +0530

    dm empty banner logic

--- src/uibits/BannerIfDmsEmpty.js
+++ src/uibits/BannerIfDmsEmpty.js
@@ -1,30 +1,76 @@
 import React from 'react';
 import {StyleSheet, View, Text, Dimensions, Image} from 'react-native';
 import {Button} from 'react-native-elements';
+import {connect} from 'react-redux';
 //import FastImage from 'react-native-fast-image';
 
 const windowHeight = Dimensions.get('window').height;
 const windowWidth = Dimensions.get('window').width;
 
+var state_here = {};
+
 function BannerIfDmsEmpty() {
-  return (
-    <View style={styles.banner_view}>
-      <Image
-        style={styles.banner_image}
-        source={require('/Users/san/Desktop/toastgo/assets/dog_3d.png')}
-      />
-      <Button
-        clear
-        buttonStyle={styles.start_club_button_style}
-        containerStyle={styles.start_club_button_container_style}
-        titleStyle={styles.start_club_button_title_style}
-        title="start talking to your friends below"
-      />
-    </View>
-  );
+  var NudgeToData = state_here.MyNudgeToListReducer.mynudgetolist;
+
+  if (NudgeToData.length > 0) {
+    if (NudgeToData[0].userid === 0) {
+      return (
+        <View style={styles.banner_view}>
+          <Image
+            style={styles.banner_image}
+            source={require('/Users/san/Desktop/toastgo/assets/dog_3d.png')}
+          />
+          <Button
+            clear
+            buttonStyle={styles.start_club_button_style}
+            containerStyle={styles.start_club_button_container_style}
+            titleStyle={styles.start_club_button_title_style}
+            title="start club to invite your friends onto Aye"
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.banner_view}>
+          <Image
+            style={styles.banner_image}
+            source={require('/Users/san/Desktop/toastgo/assets/dog_3d.png')}
+          />
+          <Button
+            clear
+            buttonStyle={styles.start_club_button_style}
+            containerStyle={styles.start_club_button_container_style}
+            titleStyle={styles.start_club_button_title_style}
+            title="start talking to your friends below"
+          />
+        </View>
+      );
+    }
+  } else {
+    return (
+      <View style={styles.banner_view}>
+        <Image
+          style={styles.banner_image}
+          source={require('/Users/san/Desktop/toastgo/assets/dog_3d.png')}
+        />
+        <Button
+          clear
+          buttonStyle={styles.start_club_button_style}
+          containerStyle={styles.start_club_button_container_style}
+          titleStyle={styles.start_club_button_title_style}
+          title="start club to invite your friends onto Aye"
+        />
+      </View>
+    );
+  }
 }
 
-export default BannerIfDmsEmpty;
+const mapStateToProps = state => {
+  state_here = state;
+  return state_here;
+};
+
+export default connect(mapStateToProps)(BannerIfDmsEmpty);
 
 const styles = StyleSheet.create({
   banner_view: {
