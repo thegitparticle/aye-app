1b592e9688202ab62b3799ceb8827d987a5d878b
SAN <12975157+thegitparticle@users.noreply.github.com>
2021-06-23 20:27:30 +0530

    pubnub 15 sec heartbeat

--- src/navigation/RootStack.js
+++ src/navigation/RootStack.js
@@ -27,6 +27,8 @@ function RootStack() {
       publishKey: 'pub-c-a65bb691-5b8a-4c4b-aef5-e2a26677122d',
       subscribeKey: 'sub-c-d099e214-9bcf-11eb-9adf-f2e9c1644994',
       uuid: state_here.MyProfileReducer.myprofile.user.id,
+      heartbeatInterval: 15,
+      // keepAlive: true,
       // logVerbosity: true,
     });
 
